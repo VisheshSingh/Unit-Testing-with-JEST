05dcab396b1ba9c942f76e38ec1b4d3a6bb9fba1
Vishesh <visheshsingh16@gmail.com>
2018-11-06 11:24:00 -0500

    Testing with toBeNull, toBeFalsy

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,22 @@ const functions = require("./functions");
 test(" Add 2 + 2 to be equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test(" Add 2 + 2 to be not equal to 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY AND FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the exact opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Shold be falsy", () => {
+  expect(functions.checkValue(0)).toBeFalsy();
+});
