e134c285e29d65fcdd715fc1f8d8ccf92b7352f3
Vishesh <visheshsingh16@gmail.com>
2018-11-06 13:11:04 -0500

    beforeEach, afterEach, beforeAll and afterAll

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,14 @@
 const functions = require("./functions");
 
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+beforeAll(() => initDatabase());
+afterAll(() => closeDatabase());
+
+const initDatabase = () => console.log("Database initialized...");
+const closeDatabase = () => console.log("Database closed...");
+
 test(" Add 2 + 2 to be equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
