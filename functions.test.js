0f038811533d63faa6db079b10d26fc64c06db26
Vishesh <visheshsingh16@gmail.com>
2018-11-06 11:39:06 -0500

    Testing eith toBeLessThan and with regex(toMatch)

--- functions.test.js
+++ functions.test.js
@@ -29,3 +29,16 @@ test("User should be Mary Jane", () => {
     lastName: "Jane"
   });
 });
+
+//Less than or greater than
+test("Should be less than 1660", () => {
+  const load1 = 800;
+  const load2 = 800;
+  //   expect(load1 + load2).toBeLessThan(1600);
+  expect(load1 + load2).toBeGreaterThanOrEqual(1600);
+});
+
+// REGEX
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
